327e66cd6a5d593ed11da71f2844b5bf5d0d937d
eluceon <goldenpoint2000@gmail.com>
2023-08-21 22:00:40 +0300

    edit

--- src/main/java/com/github/eluceon/telegram/sender/App.java
+++ src/main/java/com/github/eluceon/telegram/sender/App.java
@@ -1,4 +1,4 @@
-package com.github.eluceon.tg.sender;
+package com.github.eluceon.telegram.sender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/eluceon/telegram/sender/TgSenderApplicationTests.java
+++ src/test/java/com/github/eluceon/telegram/sender/TgSenderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.eluceon.tg.sender;
+package com.github.eluceon.telegram.sender;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TgSenderApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
