c44a8c54bcb7ef99125c52c7f1e41d5e65219a8a
eluceon <goldenpoint2000@gmail.com>
2023-08-21 23:05:49 +0300

    add entity

---
+++ src/main/java/com/github/eluceon/telegram/sender/entity/Message.java
@@ -0,0 +1,37 @@
+package com.github.eluceon.telegram.sender.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "MESSAGES")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "CONTENT")
+    @NotNull
+    private String content;
+
+    @Column(name = "CHAT_ID")
+    @NotNull
+    private String chatId;
+
+    @Column(name = "SCHEDULED_TIME")
+    @NotNull
+    private LocalTime scheduledTime;
+
+    @Column(name = "CREATED_AT")
+    @CreationTimestamp
+    private ZonedDateTime createdAt;
+}
